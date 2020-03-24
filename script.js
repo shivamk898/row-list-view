a3714c3cee2d711cf002c69d342ef0c31c3a5460
shivamiqa <shivamk898@gmail.com>
2020-03-24 16:56:51 +0530

    new files

---
+++ script.js
@@ -0,0 +1,97 @@
+// var flag = 0;
+// function toggle(){
+// if(flag==0){
+//     $("#toggleDiv").text("row-view");
+//     flag=1;
+// }
+// else if(flag==1){
+//     $("#toggleDiv").text("list-view");
+//     flag=0;
+// }
+// }
+
+
+//Users
+var User = Backbone.Model.extend({
+    initialize: function(){
+    }
+});
+
+var Users = Backbone.Collection.extend({
+    model: User, 
+    url: "https://jsonplaceholder.typicode.com/users"
+});
+
+var users = new Users();
+
+var UserView = Backbone.View.extend({
+    mode: true,
+    el: "#main-container", 
+    template1: _.template($('#user-row-template').html()),
+    template2:_.template($('#user-brick-template').html()),
+    initialize: function(){        
+    },
+    events: {
+        'click .list-view': 'myFunction',
+        'click .row-view': 'myFunction2'
+    },
+	myFunction: function() {
+        users.fetch().then(function(){
+        userView.render().$el;});
+		$('.list-view').hide();
+        $('.row-view').show();
+    },
+    myFunction2: function(){
+        users.fetch().then(function(){
+            userView1.render().$el;});
+            $('.list-view').show();
+            $('.row-view').hide();  
+    },
+    render: function(){
+        if(this.mode == true){
+            this.$el.html(this.template2({users: this.collection.toJSON()}));
+        }
+        else{
+            this.$el.html(this.template1({users: this.collection.toJSON()}));
+        }
+        var subView1 = new UserView({el: 'body'});
+        var self = this;
+		self.$el.append(this.subView1);
+        return this;
+        }
+    });
+
+var userView = new UserView({collection: users});
+
+var UserView1 = Backbone.View.extend({
+    el: "#main-container", 
+    template1: _.template($('#user-row-template').html()),
+    initialize: function(){        
+    },
+    render: function(){
+        let data = this.template1({users: this.collection.toJSON()});
+        this.$el.html(data);
+        return this;
+    }
+});
+
+var userView1 = new UserView1({collection: users});
+users.fetch().then(function(){
+    userView.render().$el;});
+
+$(".userview").on('click' , function(){
+    console.log("inside users now");    
+});
+
+// $(".list-view").on("click", myFunction);
+// function myFunction() {
+//     users.fetch().then(function(){
+//     userView.render().$el;});
+//     $('.list-view').hide();
+//     $('.row-view').show();
+//     $(".row-view").on("click", myFunctions);
+//     function myFunctions(){
+//         $('.list-view').show();
+//         $('.row-view').hide();  
+//     }
+// }
